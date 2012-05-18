5dbb89ee983eddae0609004a7262b5e0c913e2a0
Nishanth Menon <nm@ti.com>
2012-05-18 12:26:19 -0500

    ARM: OMAP2+: OPP: Fix to ensure check of right oppdef after bad one
    commit b110547e586eb5825bc1d04aa9147bff83b57672 upstream.
    
    Commit 9fa2df6b90786301b175e264f5fa9846aba81a65
    (ARM: OMAP2+: OPP: allow OPP enumeration to continue if device is not present)
    makes the logic:
    for (i = 0; i < opp_def_size; i++) {
    	<snip>
    	if (!oh || !oh->od) {
    		<snip>
    		continue;
    	}
    <snip>
    opp_def++;
    }
    
    In short, the moment we hit a "Bad OPP", we end up looping the list
    comparing against the bad opp definition pointer for the rest of the
    iteration count. Instead, increment opp_def in the for loop itself
    and allow continue to be used in code without much thought so that
    we check the next set of OPP definition pointers :)
    
    Cc: Steve Sakoman <steve@sakoman.com>
    Cc: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Kevin Hilman <khilman@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    
    	arch/arm/mach-omap2/opp.c

--- arch/arm/mach-omap2/opp.c
+++ arch/arm/mach-omap2/opp.c
@@ -66,7 +66,7 @@ int __init omap_init_opp_table(struct omap_opp_def *opp_def,
 	omap_table_init = 1;
 
 	/* Lets now register with OPP library */
-	for (i = 0; i < opp_def_size; i++) {
+	for (i = 0; i < opp_def_size; i++, opp_def++) {
 		struct omap_hwmod *oh;
 		struct device *dev;
 
@@ -124,8 +124,6 @@ int __init omap_init_opp_table(struct omap_opp_def *opp_def,
 			liveoc_register_oppdevice(dev, opp_def->hwmod_name);
 #endif
 		}
-next:
-		opp_def++;
 	}
 
 	return 0;
