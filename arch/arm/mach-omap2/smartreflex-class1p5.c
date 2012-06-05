d8948f1f5d5ba61b5cbdffce3b10b7c0b6b6707f
franciscofranco <franciscofranco.1990@gmail.com>
2012-06-05 18:33:10 +0100

    smartreflex-class1p5.c: add a routine for SR not to calibrate the MPU step if the nominal voltage is above 1400mV. Original idea from Imoseyon

--- arch/arm/mach-omap2/smartreflex-class1p5.c
+++ arch/arm/mach-omap2/smartreflex-class1p5.c
@@ -424,6 +424,14 @@ static int sr_class1p5_enable(struct voltagedomain *voltdm,
 	if (volt_data->volt_calibrated)
 		return 0;
 
+	/* Based on Imoseyon's idea to properly calibrate high frequencies e.g. >= 1.5Ghz MPU */
+	if (volt_data->volt_nominal > 1350000) {
+		volt_data->volt_calibrated = volt_data->volt_nominal;
+		volt_data->volt_dynamic_nominal = volt_data->volt_nominal;
+		pr_info("[franciscofranco] %p - nominal %d", __func__, volt_data->volt_nominal);
+		return 0;
+	}
+
 	work_data = (struct sr_class1p5_work_data *)voltdm_cdata;
 	if (IS_ERR_OR_NULL(work_data)) {
 		pr_err("%s: bad work data??\n", __func__);
