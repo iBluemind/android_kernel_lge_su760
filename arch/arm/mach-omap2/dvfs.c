ca0efa6bd303898c8ca565c94cc98f0db6b4c78d
Ezekeel <notezekeel@googlemail.com>
2012-07-06 23:59:00 +0200

    Added Custom Voltage version 3.
    Conflicts:
    
    	arch/arm/mach-omap2/omap2plus-cpufreq.c
    	drivers/misc/Makefile

--- arch/arm/mach-omap2/dvfs.c
+++ arch/arm/mach-omap2/dvfs.c
@@ -29,6 +29,10 @@
 #include "powerdomain.h"
 #include "pm.h"
 
+#ifdef CONFIG_CUSTOM_VOLTAGE
+#include <linux/custom_voltage.h>
+#endif
+
 /**
  * DOC: Introduction
  * =================
@@ -251,7 +255,9 @@ static struct omap_vdd_dvfs_info *_dev_to_dvfs_info(struct device *dev)
  *
  * Returns NULL on failure.
  */
+#ifndef CONFIG_CUSTOM_VOLTAGE
 static
+#endif
 struct omap_vdd_dvfs_info *_voltdm_to_dvfs_info(struct voltagedomain *voltdm)
 {
 	struct omap_vdd_dvfs_info *dvfs_info;
@@ -266,6 +272,9 @@ struct omap_vdd_dvfs_info *_voltdm_to_dvfs_info(struct voltagedomain *voltdm)
 
 	return NULL;
 }
+#ifdef CONFIG_CUSTOM_VOLTAGE
+EXPORT_SYMBOL(_voltdm_to_dvfs_info);
+#endif
 
 /**
  * _volt_to_opp() - Find OPP corresponding to a given voltage
@@ -1341,5 +1350,10 @@ int __init omap_dvfs_register_device(struct device *dev, char *voltdm_name,
 	/* Fall through */
 out:
 	mutex_unlock(&omap_dvfs_lock);
+
+#ifdef CONFIG_CUSTOM_VOLTAGE
+	customvoltage_register_dvfsmutex(&omap_dvfs_lock);
+#endif
+
 	return ret;
 }
