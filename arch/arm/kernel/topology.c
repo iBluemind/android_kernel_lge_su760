347abd12d09e998dc889f71c70cf905d02c79b64
Han Manjong <han@manjong.org>
2012-08-27 16:17:18 +0900

    Update ARM CPU topology definition

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -1,7 +1,8 @@
 /*
  * arch/arm/kernel/topology.c
  *
- * Copyright (C) 2011  vincent.guittot@linaro.org
+ * Copyright (C) 2011 Linaro Limited.
+ * Written by: Vincent Guittot
  *
  * based on arch/sh/kernel/topology.c
  *
@@ -18,22 +19,33 @@
 #include <linux/nodemask.h>
 #include <linux/sched.h>
 
-#include <asm/cacheflush.h>
+#include <asm/cputype.h>
 #include <asm/topology.h>
 
-#define hard_smp_mpidr() \
-	({ \
-		unsigned int cpunum; \
-		__asm__("mrc p15, 0, %0, c0, c0, 5"	\
-			: "=r" (cpunum)); \
-		cpunum; \
-	})
+#define MPIDR_SMP_BITMASK (0x3 << 30)
+#define MPIDR_SMP_VALUE (0x2 << 30)
+
+#define MPIDR_MT_BITMASK (0x1 << 24)
+
+/*
+ * These masks reflect the current use of the affinity levels.
+ * The affinity level can be up to 16 bits according to ARM ARM
+ */
+
+#define MPIDR_LEVEL0_MASK 0x3
+#define MPIDR_LEVEL0_SHIFT 0
+
+#define MPIDR_LEVEL1_MASK 0xF
+#define MPIDR_LEVEL1_SHIFT 8
+
+#define MPIDR_LEVEL2_MASK 0xFF
+#define MPIDR_LEVEL2_SHIFT 16
 
 struct cputopo_arm cpu_topology[NR_CPUS];
 
 const struct cpumask *cpu_coregroup_mask(unsigned int cpu)
 {
-	return &(cpu_topology[cpu].core_sibling);
+	return &cpu_topology[cpu].core_sibling;
 }
 
 /*
@@ -43,7 +55,7 @@ const struct cpumask *cpu_coregroup_mask(unsigned int cpu)
  */
 void store_cpu_topology(unsigned int cpuid)
 {
-	struct cputopo_arm *cpuid_topo = &(cpu_topology[cpuid]);
+	struct cputopo_arm *cpuid_topo = &cpu_topology[cpuid];
 	unsigned int mpidr;
 	unsigned int cpu;
 
@@ -51,25 +63,30 @@ void store_cpu_topology(unsigned int cpuid)
 	if (cpuid_topo->core_id != -1)
 		return;
 
-	mpidr = hard_smp_mpidr();
+	mpidr = read_cpuid_mpidr();
 
 	/* create cpu topology mapping */
-	if (mpidr & (0x3 << 30)) {
+	if ((mpidr & MPIDR_SMP_BITMASK) == MPIDR_SMP_VALUE) {
 		/*
 		 * This is a multiprocessor system
 		 * multiprocessor format & multiprocessor mode field are set
 		 */
 
-		if (mpidr & (0x1 << 24)) {
+		if (mpidr & MPIDR_MT_BITMASK) {
 			/* core performance interdependency */
-			cpuid_topo->thread_id = (mpidr & 0x3);
-			cpuid_topo->core_id =  ((mpidr >> 8) & 0xF);
-			cpuid_topo->socket_id = ((mpidr >> 16) & 0xFF);
+			cpuid_topo->thread_id = (mpidr >> MPIDR_LEVEL0_SHIFT)
+				& MPIDR_LEVEL0_MASK;
+			cpuid_topo->core_id = (mpidr >> MPIDR_LEVEL1_SHIFT)
+				& MPIDR_LEVEL1_MASK;
+			cpuid_topo->socket_id = (mpidr >> MPIDR_LEVEL2_SHIFT)
+				& MPIDR_LEVEL2_MASK;
 		} else {
-			/* normal core interdependency */
+			/* largely independent cores */
 			cpuid_topo->thread_id = -1;
-			cpuid_topo->core_id = (mpidr & 0x3);
-			cpuid_topo->socket_id = ((mpidr >> 8) & 0xF);
+			cpuid_topo->core_id = (mpidr >> MPIDR_LEVEL0_SHIFT)
+				& MPIDR_LEVEL0_MASK;
+			cpuid_topo->socket_id = (mpidr >> MPIDR_LEVEL1_SHIFT)
+				& MPIDR_LEVEL1_MASK;
 		}
 	} else {
 		/*
@@ -77,7 +94,6 @@ void store_cpu_topology(unsigned int cpuid)
 		 * we are in multiprocessor format but uniprocessor system
 		 * or in the old uniprocessor format
 		 */
-
 		cpuid_topo->thread_id = -1;
 		cpuid_topo->core_id = 0;
 		cpuid_topo->socket_id = -1;
@@ -85,7 +101,7 @@ void store_cpu_topology(unsigned int cpuid)
 
 	/* update core and thread sibling masks */
 	for_each_possible_cpu(cpu) {
-		struct cputopo_arm *cpu_topo = &(cpu_topology[cpu]);
+		struct cputopo_arm *cpu_topo = &cpu_topology[cpu];
 
 		if (cpuid_topo->socket_id == cpu_topo->socket_id) {
 			cpumask_set_cpu(cpuid, &cpu_topo->core_sibling);
@@ -104,11 +120,10 @@ void store_cpu_topology(unsigned int cpuid)
 	}
 	smp_wmb();
 
-	printk(KERN_INFO "cpu %u : thread %d cpu %d, socket %d, mpidr %x\n",
+	printk(KERN_INFO "CPU%u: thread %d, cpu %d, socket %d, mpidr %x\n",
 		cpuid, cpu_topology[cpuid].thread_id,
 		cpu_topology[cpuid].core_id,
 		cpu_topology[cpuid].socket_id, mpidr);
-
 }
 
 /*
