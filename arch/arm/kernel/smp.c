3301cc40530611eb9b657e6895393f29f2c56f0a
Will Deacon <will.deacon@arm.com>
2012-10-19 17:53:01 +0100

    ARM: 7559/1: smp: switch away from the idmap before updating init_mm.mm_count
    commit 5f40b909728ad784eb43aa309d3c4e9bdf050781 upstream.
    
    When booting a secondary CPU, the primary CPU hands two sets of page
    tables via the secondary_data struct:
    
    	(1) swapper_pg_dir: a normal, cacheable, shared (if SMP) mapping
    	    of the kernel image (i.e. the tables used by init_mm).
    
    	(2) idmap_pgd: an uncached mapping of the .idmap.text ELF
    	    section.
    
    The idmap is generally used when enabling and disabling the MMU, which
    includes early CPU boot. In this case, the secondary CPU switches to
    swapper as soon as it enters C code:
    
    	struct mm_struct *mm = &init_mm;
    	unsigned int cpu = smp_processor_id();
    
    	/*
    	 * All kernel threads share the same mm context; grab a
    	 * reference and switch to it.
    	 */
    	atomic_inc(&mm->mm_count);
    	current->active_mm = mm;
    	cpumask_set_cpu(cpu, mm_cpumask(mm));
    	cpu_switch_mm(mm->pgd, mm);
    
    This causes a problem on ARMv7, where the identity mapping is treated as
    strongly-ordered leading to architecturally UNPREDICTABLE behaviour of
    exclusive accesses, such as those used by atomic_inc.
    
    This patch re-orders the secondary_start_kernel function so that we
    switch to swapper before performing any exclusive accesses.
    
    Reported-by: Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
    Cc: David McKay <david.mckay@st.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    
    	arch/arm/kernel/smp.c

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -285,9 +285,9 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 	unsigned int cpu;
 
 	/*
-	* The identity mapping is uncached (strongly ordered), so
-	* switch away from it before attempting any exclusive accesses.
-	*/
+	 * The identity mapping is uncached (strongly ordered), so
+	 * switch away from it before attempting any exclusive accesses.
+	 */
 	cpu_switch_mm(mm->pgd, mm);
 	enter_lazy_tlb(mm, current);
 	local_flush_tlb_all();
