01bf99902ca0d4bebc4665366de70f32d94a46b4
milaq <micha.laqua@gmail.com>
2012-12-03 01:43:17 +0100

    tspdrv: Add timed interface backported from p990

--- drivers/misc/tspdrv/tspdrv.c
+++ drivers/misc/tspdrv/tspdrv.c
@@ -316,6 +316,8 @@ static int ioctl(struct inode *inode, struct file *file, unsigned int cmd, unsig
 	int i;
 #endif
 
+        VibeInt8 nForce[1] = {128};
+
 	//printk("{tspdrv} : ioctl cmd = %x\n", cmd);
 
 	switch (cmd) {
@@ -355,6 +357,12 @@ static int ioctl(struct inode *inode, struct file *file, unsigned int cmd, unsig
 	case TSPDRV_MAGIC_NUMBER:
 		file->private_data = (void*)TSPDRV_MAGIC_NUMBER;
 		break;
+
+        case TSPDRV_ENABLE_TIMED_AMP:
+                ImmVibeSPI_ForceOut_AmpEnable(0);
+                ImmVibeSPI_ForceOut_SetSamples(0, 8, 1, nForce);
+                VibeOSKernelLinuxAutoTimer(*((int*)arg));
+                break;
 
 	case TSPDRV_ENABLE_AMP:
 		ImmVibeSPI_ForceOut_AmpEnable(arg);
