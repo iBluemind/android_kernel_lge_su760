dd3c3005ddd2bab3f5784ccbe6b81914baf11a19
iBluemind <han@manjong.org>
2012-12-24 16:17:31 +0900

    Revert "lowmemorykiller: Compact memory when killing processes"
    This reverts commit 6eda58ae9cf532f7b3ec33919f5e6eaa8a667ff7.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -35,7 +35,6 @@
 #include <linux/oom.h>
 #include <linux/sched.h>
 #include <linux/notifier.h>
-#include <linux/compaction.h>
 
 //<!-- BEGIN: hyeongseok.kim@lge.com 2012-08-16 -->
 //<!-- MOD : make LMK see swap condition 
@@ -73,8 +72,6 @@ char *lmk_kill_info = 0;
 
 static struct task_struct *lowmem_deathpending;
 static unsigned long lowmem_deathpending_timeout;
-
-extern int compact_nodes();
 
 #define lowmem_print(level, x...)			\
 	do {						\
@@ -242,8 +239,6 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	read_unlock(&tasklist_lock);
-    if (selected)
-        compact_nodes();
 	return rem;
 }
 
--- mm/compaction.c
+++ mm/compaction.c
@@ -715,7 +715,7 @@ static int compact_node(int nid)
 }
 
 /* Compact all nodes in the system */
-int compact_nodes(void)
+static int compact_nodes(void)
 {
 	int nid;
 
