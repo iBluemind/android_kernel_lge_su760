8538330af137ae14bad8b3137b95c169f7f4fc60
iBluemind <han@manjong.org>
2013-01-24 22:35:04 +0900

    omap2plus-cpufreq: set transition latency to 30ms

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -499,7 +499,7 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 		cpumask_setall(policy->cpus);
 	}
 
-	policy->cpuinfo.transition_latency = 40 * 1000;
+	policy->cpuinfo.transition_latency = 30 * 1000;
 
 #ifdef CONFIG_CUSTOM_VOLTAGE
 	customvoltage_register_freqtable(freq_table);
