755d19596f8d0c281c93376eeae1fb965a24f5c5
iBluemind <han@manjong.org>
2013-01-26 11:43:34 +0900

    smartreflex-class1p5.c: Change the check to skip calib to 1375 from 1350

--- arch/arm/mach-omap2/smartreflex-class1p5.c
+++ arch/arm/mach-omap2/smartreflex-class1p5.c
@@ -428,7 +428,7 @@ static int sr_class1p5_enable(struct voltagedomain *voltdm,
 		return 0;
 
 	/* Based on Imoseyon's idea to properly calibrate high frequencies e.g. >= 1.5Ghz MPU */
-	if (volt_data->volt_nominal >= 1350000) {
+	if (volt_data->volt_nominal >= 1375000) {
 		volt_data->volt_calibrated = volt_data->volt_nominal;
 		volt_data->volt_dynamic_nominal = volt_data->volt_nominal;
 		pr_info("[franciscofranco] %p - nominal %d", __func__, volt_data->volt_nominal);
